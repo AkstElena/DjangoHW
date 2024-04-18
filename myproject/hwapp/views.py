51e30c835ba6a0b3eb1a3c446c1b307b9b138cf2
Akst Elena <akstelena@yandex.ru>
2024-04-18 09:29:06 +0300

    correct hw1

--- myproject/hwapp/views.py
+++ myproject/hwapp/views.py
@@ -19,7 +19,7 @@ def main(request):
         <head>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-            
+            <link rel="icon" href="data:;base64,=">
             <title>
                   Homework 1 - Main
             </title>
@@ -125,6 +125,7 @@ def about(request):
             <meta charset="UTF-8" />
             <meta http-equiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="icon" href="data:;base64,=">
             <title>Document</title>
           </head>
           <body>
